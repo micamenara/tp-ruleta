6bc21abafead2b7e15f0dbf4dd5149cc82b7e2c2
Micaela <micaela@whyline.com>
2019-03-26 13:00:11 -0300

    Update hist

--- histograma.py
+++ histograma.py
@@ -5,7 +5,7 @@ import matplotlib
 import matplotlib.pyplot as plt
 import random
 import math
-myarr = np.random.randint(low = 1, high = 37, size = 500)
-plt.hist(myarr, bins=36, density=True, color = "green", ec="green")
+myarr = np.random.randint(low = 0, high = 37, size = 100000)
+plt.hist(myarr, bins=37, density=True, color = "green", ec="green")
 
 plt.show()
